f0ba2d80285ec65d67a609d59e94b5c88a58879d
Zeffon <ZeffonWu@gmail.com>
2022-02-09 17:24:47 +0800

    perf: 目录结构优化

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ import Koa from 'koa';
 import Koa2Cors from 'koa2-cors';
 import KoaBodyParser from 'koa-bodyparser';
 
-const Server = new Koa();
+const app = new Koa();
 
-Server.use(Koa2Cors()).use(KoaBodyParser());
+app.use(Koa2Cors()).use(KoaBodyParser());
 
-Server.listen(3000, () => {
+app.listen(3000, () => {
   console.log('Please open 127.0.0.1:3000');
 });
