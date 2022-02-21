d1434c0995970ac6bf858f1b32d8efa9b0d5aceb
Zeffon <ZeffonWu@gmail.com>
2022-02-21 16:36:11 +0800

    chore: 更改body参数处理中间件为koa-body

--- src/core/init.ts
+++ src/core/init.ts
@@ -1,6 +1,6 @@
 import Koa from 'koa';
 import Koa2Cors from 'koa2-cors';
-import KoaBodyParser from 'koa-bodyparser';
+import KoaBody from 'koa-body';
 import { Route } from './route/route';
 import catchError from './exception';
 import InitGlobal from './global';
@@ -16,7 +16,7 @@ export default class InitManager {
   initCore() {
     InitGlobal.init(); // 全局变量和方法
     this.app.use(Koa2Cors()); // 跨域处理
-    this.app.use(KoaBodyParser()); // body数据处理
+    this.app.use(KoaBody({ multipart: true })); // body参数处理
     this.app.use(catchError); // 全局异常处理
     const router = new Route(this.app); // 路由加载
     router.init();
