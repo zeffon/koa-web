5a8a3be9df8ac981c28ce761f7f26f8f3cec8d38
Zeffon <ZeffonWu@gmail.com>
2022-02-21 16:37:10 +0800

    test: 多形式参数测试

--- src/app/api/v1/mysql.ts
+++ src/app/api/v1/mysql.ts
@@ -4,11 +4,32 @@ import { Prefix, Get, Post, Required } from '../../../core/route';
 
 @Prefix('v1')
 export default class TestMysql {
+  //  参数形式 /v1/mysql?id=1
   @Get('mysql')
   @Required(['id'])
   async mysql(ctx: Koa.Context, next: any) {
     console.log(ctx.query);
     const id = ctx.query.id;
+    let sql = `SELECT * FROM user WHERE id = ${id}`;
+    const res = await query(sql);
+    ctx.body = res;
+  }
+
+  //  参数形式 /v1/mysql/1
+  @Get('mysql/:id')
+  async mysql1(ctx: Koa.Context, next: any) {
+    console.log(ctx.params);
+    const id = ctx.params.id;
+    let sql = `SELECT * FROM user WHERE id = ${id}`;
+    const res = await query(sql);
+    ctx.body = res;
+  }
+
+  //  参数形式 /v1/mysql  json: { id: 1 }
+  @Post('mysql')
+  async mysql2(ctx: Koa.Context, next: any) {
+    console.log(ctx.request.body);
+    const id = ctx.request.body.id;
     let sql = `SELECT * FROM user WHERE id = ${id}`;
     const res = await query(sql);
     ctx.body = res;
