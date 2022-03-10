cbfa722e6f396d814ce27264497999d0585d050d
zeffon <ZeffonWu@gmail.com>
2022-03-10 02:29:03 +0000

    Deploying to build from  @ ff1f53b2baa10ce785f212ff6bb524ff1afa67cf 🚀

--- app/api/v1/test-app.js
+++ app/api/v1/test-app.js
@@ -38,8 +38,7 @@ const registerSchema = {
             new validator_1.Rule('isLength', '至少12个字符，最多32个字符', {
                 min: 6,
                 max: 32
-            }),
-            new validator_1.Rule('isEmail', '不符合Email规范')
+            })
         ]
     },
     password1: { type: 'string', required: true, rules: [] },
