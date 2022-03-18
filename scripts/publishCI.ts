a7d770f92058a7e6c51db1669b4b42ab00fb9ff4
Zeffon <ZeffonWu@gmail.com>
2022-03-18 22:58:55 +0800

    chore: release and publishCI script

---
+++ scripts/publishCI.ts
@@ -0,0 +1,34 @@
+/**
+ * modified from https://github.com/vitejs/vite/blob/main/scripts/publishCI.ts
+ */
+import { args, getPackageInfo, publishPackage, step } from './releaseUtils'
+
+async function main() {
+  const tag = args._[0]
+
+  if (!tag) {
+    throw new Error('No tag specified')
+  }
+
+  let pkgName = 'create-koa-web'
+  let version
+
+  if (tag.includes('@')) [pkgName, version] = tag.split('@')
+  else version = tag
+
+  if (version.startsWith('v')) version = version.slice(1)
+
+  const { currentVersion, pkgDir } = getPackageInfo(pkgName)
+  if (currentVersion !== version)
+    throw new Error(
+      `Package version from tag "${version}" mismatches with current version "${currentVersion}"`
+    )
+
+  step('Publishing package...')
+  await publishPackage(pkgDir, version.includes('beta') ? 'beta' : undefined)
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
