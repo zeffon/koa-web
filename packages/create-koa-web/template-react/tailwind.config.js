08f3e6c81c42f7888268ecc9734c8b8875ca20fb
Zeffon Wu <ZeffonWu@gmail.com>
2022-05-27 16:36:54 +0800

    feat: template-react add tailwindcss

---
+++ packages/create-koa-web/template-react/tailwind.config.js
@@ -0,0 +1,10 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
