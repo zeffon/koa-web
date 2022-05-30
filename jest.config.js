424ca8421d4d063876934a6363f280694919954d
Zeffon Wu <ZeffonWu@gmail.com>
2022-05-30 20:21:41 +0800

    test: update test script

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^~(.*)$': '<rootDir>packages/create-koa-web/template-koa/src$1'
+  },
   roots: [
     './packages/create-koa-web/template-koa' // jest 扫描的目录
   ]
