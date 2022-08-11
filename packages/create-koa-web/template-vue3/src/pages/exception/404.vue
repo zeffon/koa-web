72357bbe3e45510d46d0201e7e60609859126b7d
Zeffon Wu <ZeffonWu@gmail.com>
2022-08-11 14:07:22 +0800

    chore: remove vue3 and react template

--- packages/create-koa-web/template-vue3/src/pages/exception/404.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div
-    class="w-full h-screen flex flex-col items-center justify-center text-4xl bg-blue-50"
-  >
-    404页面找不到
-  </div>
-</template>
-
-<script setup lang="ts"></script>
