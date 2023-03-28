f195be6f35d1ca6f5ba3e009aa3a619627668899
Zeffon Wu <ZeffonWu@gmail.com>
2023-03-28 18:15:02 +0800

    fix(full): the log is not ouput in pm2

--- packages/create-koa-web/template-full/src/core/log/log-config.ts
+++ packages/create-koa-web/template-full/src/core/log/log-config.ts
@@ -16,32 +16,55 @@ const ERROR_LOG_PATH = BASE_LOG_PATH + ERROR_PATH + '/' + ERROR_FILE_NAME
 export default {
   appenders: {
     'rule-console': { type: 'console' },
-    infoLogger: {
+    info: {
       type: 'dateFile',
       filename: INFO_LOG_PATH,
-      pattern: '-yyyy-MM-dd-hh.log',
+      pattern: '-yyyy-MM-dd.log',
       alwaysIncludePattern: true,
       encoding: 'utf-8',
       maxLogSize: 10 * 1024 * 1024, // 10 m
       numBackups: 3,
-      path: INFO_PATH,
+      daysToKeep: 30,
     },
-    errorLogger: {
+    error: {
       type: 'dateFile',
       filename: ERROR_LOG_PATH,
-      pattern: '-yyyy-MM-dd-hh.log',
+      pattern: '-yyyy-MM-dd.log',
       alwaysIncludePattern: true,
       encoding: 'utf-8',
       maxLogSize: 10 * 1024 * 1024, // 10 m
       numBackups: 3,
-      path: ERROR_PATH,
+      daysToKeep: 30,
     },
   },
   categories: {
-    default: { appenders: ['rule-console'], level: 'all' },
-    infoLogger: { appenders: ['infoLogger'], level: 'all' },
-    errorLogger: { appenders: ['errorLogger'], level: 'all' },
-    http: { appenders: ['infoLogger'], level: 'info' },
+    default: { appenders: ['rule-console'], level: 'info' },
+    info: { appenders: ['info'], level: 'info' },
+    error: { appenders: ['error'], level: 'info' },
   },
   baseLogPath: BASE_LOG_PATH,
+  /**
+   * If you use pm2 run serve, log4js cannot generate log files.
+   * You configure the following and add pm2InstanceVar at pm2 start.json.
+   * instance_var is equal to pm2InstanceVar
+   * {
+   *   "apps": [{
+   *     "env": {
+   *       "NODE_ENV": "development"
+   *     },
+   *     "name": "koa-web",
+   *     "script": "/koa-web/dist/app.js",
+   *     "instance_var": "koa-web_log4js_3100",
+   *     "cwd": "/koa-web",
+   *     "watch": [
+   *        "dist"
+   *      ],
+   *     "error_file":"./logs/pm2/error.log",
+   *     "out_file":"./logs/pm2/out.log",
+   *     "log_date_format":"YYYY-MM-DD HH:mm:ss",
+   *   }]
+   * }
+   */
+  pm2: true,
+  pm2InstanceVar: 'koa-web_log4js_3100',
 }
