84e4f0954ee501e178d90e38cbed675931ffe62f
Zeffon Wu <ZeffonWu@gmail.com>
2023-03-31 09:28:54 +0800

    dx(template): KoaBody modify to koaBody

--- packages/create-koa-web/template-standard/src/core/init.ts
+++ packages/create-koa-web/template-standard/src/core/init.ts
@@ -1,6 +1,6 @@
 import type Koa from 'koa'
 import cors from '@koa/cors'
-import KoaBody from 'koa-body'
+import koaBody from 'koa-body'
 import catchError from './exception'
 import InitGlobal from './global'
 import swaggerRouter from './swagger'
@@ -17,7 +17,7 @@ export default class InitManager {
   initCore() {
     InitGlobal.init() // global var and methods
     this.app.use(cors()) // cross-domain processing
-    this.app.use(KoaBody({ multipart: true })) // body parameter processing
+    this.app.use(koaBody({ multipart: true })) // body parameter processing
     this.app.use(catchError) // global exception handling
     this._initRoutesAndSwagger() // router and api docs
     initDB()
