1c8991cd7f6c0f97503fbfb79c82ab68047fa16d
Zeffon Wu <ZeffonWu@gmail.com>
2023-04-18 10:35:17 +0800

    chore(template): remove model alter database

--- packages/create-koa-web/template-full/src/core/database/init.ts
+++ packages/create-koa-web/template-full/src/core/database/init.ts
@@ -1,15 +1,12 @@
 import SequelizeClient from './index'
-import CONFIG from '~/config'
 import * as models from '~/app/model'
-
-const isDev = CONFIG.ENV !== 'production'
 
 const modelsObj: { [index: string]: any } = models
 const allModel: any[] = []
 const enable = SequelizeClient.enable()
 if (enable) {
   for (const key in modelsObj) {
-    const model = modelsObj[key].sync({ alter: isDev })
+    const model = modelsObj[key].sync()
     allModel.push(model)
   }
 }
